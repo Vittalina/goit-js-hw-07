fb56d45950136af20f61f6ef19750d5acc30e323
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-06-03 18:41:17 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+const gallaryItem = galleryItems.map(({preview, original, description}) => 
+`<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+</div>`)
+        .join("");
+    
+gallery.insertAdjacentHTML("beforeend", gallaryItem);
+// console.log(gallaryItem);
+
+gallery.addEventListener('click', onImageOpen);
+
+function onImageOpen (event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+        return;
+    };
+    const instance = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+    // console.log(event.target.dataset.sourse);
+    instance.show()
+
+window.addEventListener('keydown', onEscKeyPress);
+
+function onEscKeyPress(event) {
+    if (event.code === "Escape") {
+        instance.close();
+    }
+}
+
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+const gallaryItem = galleryItems.map(({preview, original, description}) => 
+`<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`)
+        .join("");
+    
+gallery.insertAdjacentHTML("beforeend", gallaryItem);
+
+
+let galleryEl = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionType: 'attr',
+    captionData: 'alt',
+    captionDelay: '250ms',
+});
